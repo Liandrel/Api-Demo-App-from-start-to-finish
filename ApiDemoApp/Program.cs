3a41df452b28e30c072f7407f06626e5da04e589
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-28 20:59:15 +0200

    Add project files.

---
+++ ApiDemoApp/Program.cs
@@ -0,0 +1,32 @@
+using DataLibrary.Data;
+using DataLibrary.Db;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IOrderData, OrderData>();
+builder.Services.AddSingleton<IFoodData, FoodData>();
+builder.Services.AddSingleton<IDataAccess, SqlDb>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
