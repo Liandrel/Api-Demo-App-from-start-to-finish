53b70201b82aace401a6333e55ee2349b62291bc
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-28 22:00:32 +0200

    Create OrderController
    Created order controller with Post method

---
+++ ApiDemoApp/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using DataLibrary.Data;
+using DataLibrary.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDemoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderData _orderData;
+        private readonly IFoodData _foodData;
+
+        public OrderController(IOrderData orderData, IFoodData foodData)
+        {
+            _orderData = orderData;
+            _foodData = foodData;
+        }
+
+        [HttpPost]
+        [ValidateModel]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Post(OrderModel order)
+        {
+            var food = await _foodData.GetFood();
+
+            order.Total = order.Quantity * food.Where(x => order.FoodId == x.Id).First().Price;
+
+            int id = await _orderData.CreateOrder(order);
+
+            return Ok(new {Id = id});
+        }
+    }
+}
